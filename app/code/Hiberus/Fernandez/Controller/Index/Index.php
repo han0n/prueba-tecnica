d338bda6059b8f35b723d51b3805cf7f585d0ae1
han0n <carferme@hotmail.es>
2021-09-23 23:43:14 +0200

    Ejercicio 5

---
+++ app/code/Hiberus/Fernandez/Controller/Index/Index.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Hiberus\Fernandez\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        echo "Hola";
+        exit();
+    }
+}
