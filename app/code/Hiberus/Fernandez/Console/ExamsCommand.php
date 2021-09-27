7a5f3a465a78786749468a510c968404f3eec28a
han0n <carferme@hotmail.es>
2021-09-27 23:18:34 +0200

    Ejercicio 14

---
+++ app/code/Hiberus/Fernandez/Console/ExamsCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Hiberus\Fernandez\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Hiberus\Fernandez\Model\ExamFactory;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ExamsCommand extends Command
+{
+
+    protected ExamFactory $examFactory;
+
+    public function __construct(
+        ExamFactory $examFactory,
+        string $id = null
+    )
+    {
+        parent::__construct($id);
+        $this->examFactory = $examFactory;
+    }
+
+    public function getMarks() {
+
+        $examModel = $this->examFactory->create();
+
+        $examCollection = $examModel->getCollection();
+        return $examCollection;
+
+    }
+
+    protected function configure()
+    {
+        $this->setName('hiberus:fernandez')
+            ->setDescription('Mostrar exámenes');
+
+        parent::configure();
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $exams = $this->getMarks();//Se obtiene la colección de exámenes
+
+        $cabecera = sprintf("%1s | %5s | %-15s | %s", 'ID', 'Nota', 'Nombre', 'Apellido');
+        $output->writeln('<info>' . $cabecera . '</info>');
+
+
+        foreach($exams as $exam){
+
+            $id = $exam->getIdExam();
+            $firstname = $exam->getFirstName();
+            $lastname = $exam->getLastName();
+            $mark = $exam->getMark();
+            $registro = sprintf("%2d | %5.2f | %-15s | %-10s", $id, $mark, $firstname, $lastname);
+            $output->writeln( $registro );
+        }
+
+
+    }
+
+}
