34a3dbbaa2e7b20b2989dcd17931f437e4551782
han0n <carferme@hotmail.es>
2021-09-30 04:27:54 +0200

    Ejercicio 16

--- app/code/Hiberus/Fernandez/Block/Index.php
+++ app/code/Hiberus/Fernandez/Block/Index.php
@@ -3,18 +3,39 @@
 namespace Hiberus\Fernandez\Block;
 
 use Hiberus\Fernandez\Model\ExamFactory;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
 
 class Index extends \Magento\Framework\View\Element\Template
 {
 
     protected $examFactory;
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected ScopeConfigInterface $scopeConfig;
 
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
-        ExamFactory $examFactory
+        ExamFactory $examFactory,
+        ScopeConfigInterface $scopeConfig
     ) {
         parent::__construct($context);
         $this->examFactory = $examFactory;
+        $this->scopeConfig =$scopeConfig;
+    }
+
+    public function getOK(){
+        $ok = $this->scopeConfig->getValue('hiberus_exams/config/ok_config', ScopeInterface::SCOPE_STORE);
+        if ($ok == null){
+            $ok = 5.0;
+        }
+        return $ok;
+    }
+
+    public function getMax(){
+        $max = $this->scopeConfig->getValue('hiberus_exams/config/max_config', ScopeInterface::SCOPE_STORE);
+        return $max;
     }
 
     public function getMarks() {
